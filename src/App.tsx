b848e75255c2a15d93d9a069ec45bde664398403
Oscar Carvajal <oscar.carvajal@mercadolibre.cl>
2022-02-02 14:29:09 -0300

    Add VideoJS component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import VideoJS from './Video' // point to where the functional component is stored
 
 function App() {
+  const playerRef = React.useRef(null);
+
+  const videoJsOptions = { // lookup the options in the docs for more options
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [{
+      src: '/path/to/video.mp4',
+      type: 'video/mp4'
+    }]
+  }
+
+  const handlePlayerReady = (player: any) => {
+    playerRef.current = player;
+
+    // you can handle player events here
+    player.on('waiting', () => {
+      console.log('player is waiting');
+    });
+
+    player.on('dispose', () => {
+      console.log('player will dispose');
+    });
+  };
+
+  // const changePlayerOptions = () => {
+  //   // you can update the player through the Video.js player instance
+  //   if (!playerRef.current) {
+  //     return;
+  //   }
+  //   // [update player through instance's api]
+  //   playerRef.current.src([{src: 'http://ex.com/video.mp4', type: 'video/mp4'}]);
+  //   playerRef.current.autoplay(false);
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>Rest of app here</div>
+
+      <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+
+      <div>Rest of app here</div>
+    </>
   );
 }
 
